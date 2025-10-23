286caf3fcf29a3465b3a89243a5f3f0f4a8a2de7
Volodymyr Sapiha <vsapiha@gmail.com>
2025-10-23 14:57:27 +0200

    update

--- address_resolver/src/server.py
+++ address_resolver/src/server.py
@@ -9,7 +9,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     use_ssl = app.config['ELASTICSEARCH_URL'].startswith("https")
     verify_certs = app.config['ELASTICSEARCH_VALIDATE_CERTS'].lower() == "true"
-    app.elasticsearch = Elasticsearch(hosts=[app.config['ELASTICSEARCH_URL']], http_auth=(app.config['ELASTICSEARCH_USER'],app.config['ELASTICSEARCH_PASSWORD']), timeout=60, use_ssl=use_ssl, verify_certs=verify_certs)
+    app.elasticsearch = Elasticsearch(hosts=[app.config['ELASTICSEARCH_URL']], http_auth=(app.config['ELASTICSEARCH_USER'], app.config['ELASTICSEARCH_PASSWORD']), timeout=60, use_ssl=use_ssl, verify_certs=verify_certs)
     app.register_blueprint(api_bp, url_prefix='/api')
     apm = ElasticAPM(app)
     return app
--- docker/training/old/client/sourcemaps/README.md
+++
@@ -1,1 +0,0 @@
-During startup, the opbeans-node container will copy sourcemaps into this folder.
